33a020876bad86c62ea5b1f006fa0fc4ee7f56da
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2026-02-16 21:36:57 +0900

    [Bronze I] Title: 소인수분해, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,24 @@
+def prime_factorization(n):
+    factors = []
+
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+
+    i = 3
+    while i * i <= n: # i <= math.sqrt(n)
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+        i += 2
+
+    if n > 2:
+        factors.append(n)
+
+    return factors
+
+n = int(input())
+numlist= prime_factorization(n)
+
+for num in numlist:
+    print(num)
